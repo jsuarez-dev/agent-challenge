3f1bc2aa59349a790263b27a53be3ec43a89133d
Johan Suarez Largo <jhoanseb1992@gmail.com>
2024-06-11 21:26:36 +1000

    adding playwright

--- WebConnection/Program.cs
+++ WebConnection/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Microsoft.Playwright;
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
+var page = await browser.NewPageAsync();
+await page.GotoAsync("https://playwright.dev/dotnet");
+await page.ScreenshotAsync(new() { Path = "screenshot1992.png" });
