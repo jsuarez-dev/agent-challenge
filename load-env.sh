4655e00599706bdc1c3026a6b4f3698593335349
Johan Suarez <jhoanseb1992@gmail.com>
2024-07-23 21:20:05 +1000

    fixing script of add env into secrets

--- load-env.sh
+++ load-env.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # Load environment variables from .env file and set them as user secrets
+project=$1
+
+if [ -z "$project" ]; then
+  echo "you need to selecte a project target to add the secrets"
+  exit 1
+fi
+
 if [ -f ".env" ]; then
+  dotnet user-secrets init --project "$project"
   while IFS='=' read -r key value; do
     if [[ $key != \#* ]]; then
       key=$(echo $key | xargs)
       value=$(echo $value | xargs)
-      dotnet user-secrets set "$key" "$value" --project WebConnection
+      dotnet user-secrets set "$key" "$value" --project "$project"
     fi
   done < .env
 else
